50c51d5d74c2cfa1b76ce00ac8b7f1936d411c00
arm61 <arm61@bath.ac.uk>
2018-12-06 10:27:06 +0000

    update ljpy

--- reports/code_blocks/lennardjones.py
+++ reports/code_blocks/lennardjones.py
@@ -29,9 +29,9 @@ def lj_energy(coordinates, cell, cut_off, A, B):
     energy = np.zeros((coordinates.shape[0]))
     for i in range(coordinates.shape[0] - 1):
         for j in range(i + 1, coordinates.shape[0]):
-            d = positions[j] - positions[i]
+            d = coordinates[j] - coordinates[i]
             d = d % cell
-            r = np.sqrt(np.sum(np.squared(d)))
+            r = np.sqrt(np.sum(np.square(d)))
             if r > cut_off:
                 break
             else:
